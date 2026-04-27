a1071047b5625203777848b98c8b646fae15ebd9
ChitraKalra17 <kalrachitra17@gmail.com>
2026-04-28 02:02:55 +0530

    fixed vercel routing

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -10,7 +10,8 @@ const Login = ({ onLogin }) => {
         e.preventDefault();
 
         try {
-            const res = await fetch("http://localhost:5000/auth/login", {
+            const API = import.meta.env.VITE_API_URL;
+            const res = await fetch(`${API}/auth/login`, {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
